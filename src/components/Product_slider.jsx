bcaea985bbbe9a0942631d1e18da4c6a18f434fd
Karoly <hornyal.mobile@gmail.com>
2023-07-23 21:23:37 +0100

    update

--- src/components/Product_slider.jsx
+++ src/components/Product_slider.jsx
@@ -7,20 +7,48 @@ import SectionDevider from './SectionDevider';
 export default function ProductSlider(props) {
 
     const [clicks, setClicks] = useState(0);
-    const [width, setWidth] = useState(0);
+    //const [boxWidth, setBoxWidth] = useState(0);
+    const [position, setPosition] = useState(0);
+
+    const updateMaxCount = () => {
+        let windowWidth = window.innerWidth;
+        if (windowWidth <= 650) {
+            //console.log(windowWidth, '<= 650');
+            setPosition(-6);
+        };
+        if (650 < windowWidth && windowWidth <= 1000) {
+            //console.log('650 <', windowWidth, '&&', windowWidth, '<= 1000');
+            setPosition(-5);
+        };
+        if (1000 < windowWidth && windowWidth <= 1250) {
+            //console.log('1000 <', windowWidth, '&&', windowWidth, '<= 1250');
+            setPosition(-4);
+        };
+        if (1250 < windowWidth) {
+            //console.log('1250 <', windowWidth);
+            setPosition(-3);
+        };
+    };
 
     const [marginLeft, setMarginLeft] = useState(0);
 
+    useEffect(() => {
+        const handleResize = () => {
+            updateMaxCount();
+            const firstWrapper = document.querySelector(".slider-box .wrapper:first-child");
+            const width = parseFloat(getComputedStyle(firstWrapper).width);
+            const offset = width * clicks;
+            setMarginLeft(offset);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [clicks]);
 
-    window.addEventListener('resize', () => {
-        let firstWrapper = document.querySelector(".slider-box .wrapper:first-child");
-        let width = parseFloat(getComputedStyle(firstWrapper).width);
-        let offset = width * clicks;
-        setMarginLeft(offset);
-    });
 
     const clickOnRight = () => {
-        if (clicks > -3) {
+        if (clicks > position) {
             let firstWrapper = document.querySelector(".slider-box .wrapper:first-child");
             let width = parseFloat(getComputedStyle(firstWrapper).width);
             setMarginLeft(width * (clicks - 1));
@@ -41,7 +69,7 @@ export default function ProductSlider(props) {
             <SectionDevider h2={'Termékeim'} />
 
             <div className="slider-container">
-                <div className='btn-box'><NavArrow className='icon left' onClick={clickOnLeft} style={clicks >= 0 ? { pointerEvents: 'none', opacity: '0' } : {}} /></div>
+                <div className='btn-box'><NavArrow className='icon left' onClick={clickOnLeft} style={clicks === 0 ? { pointerEvents: 'none', opacity: '0' } : {}} /></div>
 
                 <div className="slider-box">
                     {props.termekek.data.map((product, productIndex) =>
@@ -60,7 +88,7 @@ export default function ProductSlider(props) {
                     )}
                 </div>
 
-                <div className='btn-box'><NavArrow className='icon' onClick={clickOnRight} style={clicks <= -3 ? { pointerEvents: 'none', opacity: '0' } : {}} /></div>
+                <div className='btn-box'><NavArrow className='icon' onClick={clickOnRight} style={clicks === position ? { pointerEvents: 'none', opacity: '0' } : {}} /></div>
             </div>
 
         </div>
